54ce7203d364925646a1777e6765d7aa30c64db0
Helena <helenapcarrera@gmail.com>
2025-06-20 14:39:08 +0200

    entry created

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <header>
-            <img src="/img/globe.png" alt="globe-icon"/>
+            <img src="/img/Globe.png" alt="globe-icon"/>
             <h1>my travel journal.</h1>
         </header>
     )
